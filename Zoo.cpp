3a42ec173ca01ed5d657963b200dd124a8b7158f
Comp2Acc <Comp2Acc@users.noreply.github.com>
2017-09-14 14:27:51 -0400

    Added Implementation File

---
+++ Zoo.cpp
@@ -0,0 +1,27 @@
+#include "Zoo.h"
+
+Zoo::Zoo(string a, int c)
+{
+	animals = a;
+	num = c;
+}
+
+void Zoo::setName(string a)
+{
+	animals = a;
+}
+
+string Zoo::getName()
+{
+	return animals;
+}
+
+void Zoo::setNumber(int c)
+{
+	number = c;
+}
+
+int Zoo::getNumber()
+{
+	return num;
+}
