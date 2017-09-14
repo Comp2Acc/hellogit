d7524184e9cc2618c81e6299673c3ca53921819e
Comp2Acc <Comp2Acc@users.noreply.github.com>
2017-09-14 14:21:33 -0400

    Added header file

---
+++ Zoo.h
@@ -0,0 +1,18 @@
+#ifndef ZOO_H
+#define ZOO_H
+
+class Zoo
+{
+	public:
+		Zoo (string a, int c);
+		void setName(string);
+		string getName();
+		void setNUmber(int);
+		int getNumber();
+		
+	private:
+		string animals;
+		int num:
+};
+
+#endif
